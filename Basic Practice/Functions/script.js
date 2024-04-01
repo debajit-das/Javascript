254e2ae6950bb43ddd2505c51e46a8aeaca88c73
Debajit Das <debas1118@gmail.com>
2024-04-01 20:38:24 +0530

    Factorial of a number

---
+++ Basic Practice/Functions/script.js
@@ -0,0 +1,11 @@
+/*
+ * Create the function factorial here
+ */
+function factorial(n) {
+  let fact = 1;
+  for (let i = 1; i <= n; i++) {
+    fact = fact * i;
+  }
+  return fact;
+}
+factorial(4);
