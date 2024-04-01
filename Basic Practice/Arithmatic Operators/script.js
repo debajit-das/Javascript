0f2817689f2297b60de5b49718b3aa788ac9995e
Debajit Das <debas1118@gmail.com>
2024-04-01 19:48:07 +0530

    Arithmatic Operators

---
+++ Basic Practice/Arithmatic Operators/script.js
@@ -0,0 +1,43 @@
+/**
+ *   Calculate the area of a rectangle.
+ *
+ *   length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the rectangle's area.
+ **/
+
+function getArea(length, width) {
+  let area;
+  // Write your code here
+  area = length * width;
+  return area;
+}
+getArea(3, 4.5);
+// console.log(area);
+/**
+ *   Calculate the perimeter of a rectangle.
+ *
+ *	length: The length of the rectangle.
+ *   width: The width of the rectangle.
+ *
+ *	Return a number denoting the perimeter of a rectangle.
+ **/
+
+function getPerimeter(length, width) {
+  let perimeter;
+  // Write your code here
+  const x = length + width;
+  perimeter = 2 * x;
+  return perimeter;
+}
+getPerimeter(3, 4.5);
+// console.log(perimeter);
+
+function main() {
+  const length = +readLine();
+  const width = +readLine();
+
+  console.log(getArea(length, width));
+  console.log(getPerimeter(length, width));
+}
