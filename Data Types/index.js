9b2106f7d172d8116d8de0237e4175e4e05f0961
Debajit Das <debas1118@gmail.com>
2024-04-13 21:45:47 +0530

    Data Types

---
+++ Data Types/index.js
@@ -0,0 +1,22 @@
+// Example of Primitive Data Types
+
+let mobile = 9876543210;
+let schoolName = "A.C Institution";
+let magicnumber;
+let isFinite = true;
+let sym = Symbol(2);
+let bigNumber = 43545454n;
+console.log(typeof mobile);
+console.log(typeof schoolName);
+console.log(typeof magicnumber);
+console.log(typeof isFinite);
+console.log(typeof sym);
+console.log(typeof bigNumber);
+
+// Example of Non-Primitive Data Types
+
+let person = { name: "Debajit" };
+function sum() {}
+
+console.log(typeof person);
+console.log(typeof sum);
